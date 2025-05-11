b1503aa28d99fb0069a2a043982e09edc8c89143
veto9292 <veto9292@gmail.com>
2025-05-11 08:45:25 +0330

    improve/ add showWarpLicense & showWarpConfig dialog to dialog_notifier

--- lib/core/router/dialog/dialog_notifier.dart
+++ lib/core/router/dialog/dialog_notifier.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:hiddify/core/router/app_router.dart';
 import 'package:hiddify/features/common/qr_code_scanner_screen.dart';
+import 'package:hiddify/features/config_option/overview/warp_options_widgets.dart';
 import 'package:hiddify/features/profile/overview/profiles_overview_page.dart';
 import 'package:riverpod_annotation/riverpod_annotation.dart';
 
@@ -26,6 +27,24 @@ class DialogNotifier extends _$DialogNotifier {
     return await showDialog<T>(
       context: context,
       builder: (context) => const SortProfilesDialog(),
+    );
+  }
+
+  Future<T?> showWarpLicense<T>() async {
+    final context = rootNavigatorKey.currentContext;
+    if (context == null) return null;
+    return await showDialog<T>(
+      context: context,
+      builder: (context) => const WarpLicenseDialog(),
+    );
+  }
+
+  Future<T?> showWarpConfig<T>(String content) async {
+    final context = rootNavigatorKey.currentContext;
+    if (context == null) return null;
+    return await showDialog<T>(
+      context: context,
+      builder: (context) => WarpConfigDialog(content: content),
     );
   }
 }
