539cc7cc7a4737f9c32eca4875848b36639c7640
veto9292 <veto9292@gmail.com>
2025-06-15 01:04:13 +0330

    using dialog notifier, moving setting_checkbox to dialog/widgets

--- lib/features/route_rules/widget/setting_checkbox.dart
+++ lib/features/route_rules/widget/setting_checkbox.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:hiddify/core/localization/translations.dart';
-import 'package:hiddify/features/route_rules/notifier/rule_notifier.dart';
+import 'package:hiddify/core/router/dialog/dialog_notifier.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:protobuf/protobuf.dart';
 
@@ -29,77 +29,15 @@ class SettingCheckbox extends ConsumerWidget {
       title: Text(title),
       subtitle: Text(textWithTranslation(selectedValues, ref)),
       onTap: () async {
-        final result = await showDialog(
-          context: context,
-          builder: (context) => SettingCheckboxDialog(title: title, values: values, selectedValues: selectedValues, defaultValue: defaultValue, t: t),
-        );
+        final result = await ref.read(dialogNotifierProvider.notifier).showSettingCheckbox(
+              title: title,
+              values: values,
+              selectedValues: selectedValues,
+              defaultValue: defaultValue,
+              t: t,
+            );
         if (result is List<ProtobufEnum>) setValue(result);
       },
-    );
-  }
-}
-
-class SettingCheckboxDialog extends ConsumerWidget {
-  const SettingCheckboxDialog({
-    super.key,
-    required this.title,
-    required this.values,
-    required this.selectedValues,
-    this.defaultValue,
-    this.t,
-  });
-
-  final String title;
-  final List<ProtobufEnum> values;
-  final List<ProtobufEnum> selectedValues;
-  final List<ProtobufEnum>? defaultValue;
-  final Map<String, String>? t;
-
-  String textWithTranslation(ProtobufEnum e) {
-    if (t == null) return '$e';
-    return t!['$e'] ?? '$e';
-  }
-
-  @override
-  Widget build(BuildContext context, WidgetRef ref) {
-    final t = ref.watch(translationsProvider).requireValue;
-    final checkboxNotififier = dialogCheckboxNotifierProvider(selectedValues);
-    final current = ref.watch(checkboxNotififier);
-
-    return AlertDialog(
-      title: Text(title),
-      content: ConstrainedBox(
-        constraints: const BoxConstraints(maxHeight: 300),
-        child: SingleChildScrollView(
-          child: Column(
-            mainAxisSize: MainAxisSize.min,
-            children: values
-                .map(
-                  (e) => CheckboxListTile(
-                    title: Text(textWithTranslation(e)),
-                    value: current.contains(e),
-                    onChanged: (_) => ref.read(checkboxNotififier.notifier).update(e),
-                  ),
-                )
-                .toList(),
-          ),
-        ),
-      ),
-      actions: [
-        if (defaultValue != null)
-          TextButton(
-            child: Text(t.general.reset),
-            onPressed: () => Navigator.of(context).pop(defaultValue),
-          ),
-        TextButton(
-          child: Text(t.general.cancel),
-          onPressed: () => Navigator.of(context).pop(),
-        ),
-        TextButton(
-          child: Text(t.general.done),
-          onPressed: () => Navigator.of(context).pop(current),
-        ),
-      ],
     );
   }
 }
