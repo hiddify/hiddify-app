aae61cfbc4ce4614b2c60b2dda1cfe6999730517
veto9292 <veto9292@gmail.com>
2025-06-14 22:17:07 +0330

    fix imports

--- lib/features/connection/data/connection_data_providers.dart
+++ lib/features/connection/data/connection_data_providers.dart
@@ -1,5 +1,5 @@
 import 'package:hiddify/core/directories/directories_provider.dart';
-import 'package:hiddify/features/config_option/data/config_option_data_providers.dart';
+import 'package:hiddify/features/settings/data/config_option_data_providers.dart';
 
 import 'package:hiddify/features/connection/data/connection_repository.dart';
 
