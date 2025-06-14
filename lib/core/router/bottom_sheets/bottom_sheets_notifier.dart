4470893beded9a99a3754609de0316daf7cd51e5
veto9292 <veto9292@gmail.com>
2025-06-14 16:13:13 +0330

    fix typo(buttom to bottom), adding the _show method for managing the number of popups, reducing boilerplate code, and managing context.

--- lib/core/router/bottom_sheets/bottom_sheets_notifier.dart
+++ lib/core/router/bottom_sheets/bottom_sheets_notifier.dart
@@ -1,69 +1,56 @@
 import 'package:flutter/material.dart';
 import 'package:hiddify/core/model/constants.dart';
-import 'package:hiddify/core/router/app_router.dart';
-import 'package:hiddify/features/config_option/widget/quick_settings_modal.dart';
+import 'package:hiddify/core/router/dialog/dialog_notifier.dart';
+import 'package:hiddify/core/router/go_router/routing_config_notifier.dart';
 import 'package:hiddify/features/profile/add/add_profile_modal.dart';
 import 'package:hiddify/features/profile/overview/profiles_overview_page.dart';
+import 'package:hiddify/features/settings/widget/quick_settings_modal.dart';
 import 'package:riverpod_annotation/riverpod_annotation.dart';
 
 part 'bottom_sheets_notifier.g.dart';
 
 @riverpod
-class ButtomSheetsNotifier extends _$ButtomSheetsNotifier {
+class BottomSheetsNotifier extends _$BottomSheetsNotifier {
   @override
   void build() {}
 
-  // Wait for result example
-  // Future<T?> show<T>() async {
-  //   final context = rootNavigatorKey.currentContext;
-  //   if (context == null) return null;
-  //   return await showModalBottomSheet<T>();
-  // }
-
-  void showAddProfile({String? url}) {
-    final context = rootNavigatorKey.currentContext;
-    if (context == null) return;
-    showModalBottomSheet(
-      constraints: BottomSheetConst.boxConstraints,
-      isScrollControlled: true,
-      context: context,
-      builder: (context) => ClipRRect(
-        borderRadius: BottomSheetConst.borderRadius,
-        child: Material(
-          child: AddProfileModal(url: url),
+  Future<T?> _show<T>({required Widget child, required bool isScrollControlled}) async {
+    final context = branchNavKey.currentContext;
+    if (context == null) return null;
+    ref.read(popupCountNotifierProvider.notifier).increase();
+    return await Navigator.of(context)
+        .push<T>(
+      ModalBottomSheetRoute(
+        constraints: BottomSheetConst.boxConstraints,
+        isScrollControlled: isScrollControlled,
+        builder: (context) => ClipRRect(
+          borderRadius: BottomSheetConst.borderRadius,
+          child: Material(
+            child: child,
+          ),
         ),
       ),
+    )
+        .then(
+      (value) {
+        ref.read(popupCountNotifierProvider.notifier).decrease();
+        return value;
+      },
     );
   }
 
-  void showProfilesOverview() {
-    final context = rootNavigatorKey.currentContext;
-    if (context == null) return;
-    showModalBottomSheet(
-      constraints: BottomSheetConst.boxConstraints,
-      isScrollControlled: true,
-      context: context,
-      builder: (context) => const ClipRRect(
-        borderRadius: BottomSheetConst.borderRadius,
-        child: Material(
-          child: ProfilesOverviewModal(),
-        ),
-      ),
-    );
-  }
+  Future<void> showAddProfile({String? url}) async => await _show(
+        isScrollControlled: true,
+        child: AddProfileModal(url: url),
+      );
 
-  void showQuickSettings() {
-    final context = rootNavigatorKey.currentContext;
-    if (context == null) return;
-    showModalBottomSheet(
-      constraints: BottomSheetConst.boxConstraints,
-      context: context,
-      builder: (context) => const ClipRRect(
-        borderRadius: BottomSheetConst.borderRadius,
-        child: Material(
-          child: QuickSettingsModal(),
-        ),
-      ),
-    );
-  }
+  Future<void> showProfilesOverview() async => await _show(
+        isScrollControlled: true,
+        child: const ProfilesOverviewModal(),
+      );
+
+  Future<void> showQuickSettings() async => await _show(
+        isScrollControlled: false,
+        child: const QuickSettingsModal(),
+      );
 }
