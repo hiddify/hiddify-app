a0bb5c6318baf56ba8c92e45b80164e725dccc7c
veto9292 <veto9292@gmail.com>
2025-07-23 16:08:59 +0330

    Improving naming and logic in 'per_app_proxy_page.dart' and adding auto selection, share, import/export to the menu, sorting selected items, using menu for mode selection, setting 'hideSystemApps' with a chip.

--- lib/features/per_app_proxy/overview/per_app_proxy_page.dart
+++ lib/features/per_app_proxy/overview/per_app_proxy_page.dart
@@ -2,13 +2,14 @@ import 'package:dartx/dartx.dart';
 import 'package:fluentui_system_icons/fluentui_system_icons.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
+import 'package:gap/gap.dart';
 import 'package:go_router/go_router.dart';
 import 'package:hiddify/core/localization/translations.dart';
+import 'package:hiddify/core/model/region.dart';
 import 'package:hiddify/core/preferences/general_preferences.dart';
-import 'package:hiddify/core/widget/adaptive_icon.dart';
-
 import 'package:hiddify/features/per_app_proxy/model/per_app_proxy_mode.dart';
 import 'package:hiddify/features/per_app_proxy/overview/per_app_proxy_notifier.dart';
+import 'package:hiddify/features/settings/data/config_option_repository.dart';
 import 'package:hiddify/utils/utils.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:installed_apps/index.dart';
@@ -18,50 +19,58 @@ class PerAppProxyPage extends HookConsumerWidget with PresLogger {
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
+    final theme = Theme.of(context);
     final t = ref.watch(translationsProvider).requireValue;
     final localizations = MaterialLocalizations.of(context);
 
-    final asyncPackages = ref.watch(installedPackagesInfoProvider);
-    final asyncPackagesExcludeSystem = ref.watch(installedPackagesInfoExcludeSystemProvider);
-    final perAppProxyMode = ref.watch(Preferences.perAppProxyMode);
-    final perAppProxyList = ref.watch(perAppProxyListProvider);
+    final apps = ref.watch(appsProvider);
+    final appsHideSystem = ref.watch(appsHideSystemProvider);
 
-    final showSystemApps = useState(true);
+    final selectedApps = ref.watch(selectedAppsFilteredByModeProvider);
+    final mode = ref.watch(Preferences.perAppProxyMode);
+
+    final hideSystemApps = useState(false);
     final isSearching = useState(false);
     final searchQuery = useState("");
 
-    final filteredPackages = useMemoized(
+    final filteredPackages = useMemoized<AsyncValue<List<AppInfo>>>(
       () {
-        if (showSystemApps.value && searchQuery.value.isBlank) {
-          return showSystemApps.value ? asyncPackages : asyncPackagesExcludeSystem;
-        }
-        if (showSystemApps.value) {
-          return asyncPackages.whenData(
+        final appsFilteredByHideSystem = hideSystemApps.value ? appsHideSystem : apps;
+        if (searchQuery.value.isBlank) {
+          return appsFilteredByHideSystem.whenData(
             (value) {
-              Iterable<AppInfo> result = value;
-              if (!searchQuery.value.isBlank) {
-                result = result.filter(
-                  (e) => e.name.toLowerCase().contains(searchQuery.value.toLowerCase()),
-                );
-              }
-              return result.toList();
-            },
-          );
-        } else {
-          return asyncPackagesExcludeSystem.whenData(
-            (value) {
-              Iterable<AppInfo> result = value;
-              if (!searchQuery.value.isBlank) {
-                result = result.filter(
-                  (e) => e.name.toLowerCase().contains(searchQuery.value.toLowerCase()),
-                );
-              }
-              return result.toList();
+              value.sort(
+                (a, b) {
+                  final aInSelected = selectedApps.contains(a.packageName);
+                  final bInSelected = selectedApps.contains(b.packageName);
+                  if (aInSelected && !bInSelected) return -1;
+                  if (!aInSelected && bInSelected) return 1;
+                  return 0;
+                },
+              );
+              return value;
             },
           );
         }
+        return appsFilteredByHideSystem.whenData(
+          (value) {
+            Iterable<AppInfo> result = value;
+            if (!searchQuery.value.isBlank) {
+              result = result.filter(
+                (e) => e.name.toLowerCase().contains(searchQuery.value.toLowerCase()),
+              );
+            }
+            return result.toList();
+          },
+        );
       },
-      [asyncPackages, showSystemApps.value, searchQuery.value],
+      [
+        apps,
+        appsHideSystem,
+        hideSystemApps.value,
+        searchQuery.value,
+        mode,
+      ],
     );
 
     return Scaffold(
@@ -99,90 +108,164 @@ class PerAppProxyPage extends HookConsumerWidget with PresLogger {
                   onPressed: () => isSearching.value = true,
                   tooltip: localizations.searchFieldLabel,
                 ),
-                PopupMenuButton(
-                  icon: Icon(AdaptiveIcon(context).more),
-                  itemBuilder: (context) {
-                    return [
-                      PopupMenuItem(
-                        child: Text(
-                          showSystemApps.value ? t.settings.network.hideSystemApps : t.settings.network.showSystemApps,
+                MenuAnchor(
+                  menuChildren: <Widget>[
+                    SubmenuButton(
+                      menuChildren: <Widget>[
+                        MenuItemButton(
+                          child: Text(t.settings.network.import.Clipboard),
+                          onPressed: () async => await ref.read(selectedAppsFilteredByModeProvider.notifier).importFromClipboard(),
                         ),
-                        onTap: () => showSystemApps.value = !showSystemApps.value,
+                        MenuItemButton(
+                          child: Text(t.settings.network.import.JsonFile),
+                          onPressed: () async => await ref.read(selectedAppsFilteredByModeProvider.notifier).importFromJsonFile(),
+                        ),
+                      ],
+                      child: Text(t.general.import),
+                    ),
+                    SubmenuButton(
+                      menuChildren: <Widget>[
+                        MenuItemButton(
+                          child: Text(t.settings.network.export.Clipboard),
+                          onPressed: () async => await ref.read(selectedAppsFilteredByModeProvider.notifier).exportJsonClipboard(),
+                        ),
+                        MenuItemButton(
+                          child: Text(t.settings.network.export.JsonFile),
+                          onPressed: () async => await ref.read(selectedAppsFilteredByModeProvider.notifier).exportJsonFile(),
+                        ),
+                      ],
+                      child: Text(t.general.export),
+                    ),
+                    if (ref.watch(ConfigOptions.region) != Region.other) ...[
+                      MenuItemButton(
+                        child: Text(t.general.share),
+                        onPressed: () async => await ref.read(selectedAppsFilteredByModeProvider.notifier).share(),
                       ),
-                      PopupMenuItem(
-                        child: Text(t.settings.network.clearSelection),
-                        onTap: () => ref.read(perAppProxyListProvider.notifier).update([]),
+                      const PopupMenuDivider(),
+                      MenuItemButton(
+                        child: Text(t.settings.network.autoSelection.title),
+                        onPressed: () async => await ref.read(selectedAppsFilteredByModeProvider.notifier).autoSelection(),
                       ),
-                    ];
-                  },
+                    ],
+                    MenuItemButton(
+                      child: Text(t.settings.network.clearSelection),
+                      onPressed: () => ref.read(selectedAppsFilteredByModeProvider.notifier).clearSelection(),
+                    ),
+                  ],
+                  builder: (context, controller, child) => IconButton(
+                    onPressed: () {
+                      if (controller.isOpen) {
+                        controller.close();
+                      } else {
+                        controller.open();
+                      }
+                    },
+                    icon: const Icon(Icons.more_vert_rounded),
+                  ),
                 ),
               ],
-            ),
-      body: Column(
-        children: [
-          ...PerAppProxyMode.values.map(
-            (e) => RadioListTile<PerAppProxyMode>(
-              title: Text(e.present(t).message),
-              dense: true,
-              value: e,
-              groupValue: perAppProxyMode,
-              onChanged: (value) async {
-                await ref.read(Preferences.perAppProxyMode.notifier).update(e);
-                if (e == PerAppProxyMode.off && context.mounted) {
-                  context.pop();
-                }
-              },
-            ),
-          ),
-          const Divider(height: 1),
-          Expanded(
-            child: filteredPackages.when(
-              data: (packages) => ListView.builder(
-                itemBuilder: (context, index) {
-                  final package = packages[index];
-                  final selected = perAppProxyList.contains(package.packageName);
-                  return CheckboxListTile(
-                    title: Text(
-                      package.name,
-                      overflow: TextOverflow.ellipsis,
-                    ),
-                    subtitle: Text(
-                      package.packageName,
-                      style: Theme.of(context).textTheme.bodySmall,
-                    ),
-                    value: selected,
-                    onChanged: (value) async {
-                      final List<String> newSelection;
-                      if (selected) {
-                        newSelection = perAppProxyList.exceptElement(package.packageName).toList();
-                      } else {
-                        newSelection = [
-                          ...perAppProxyList,
-                          package.packageName,
-                        ];
-                      }
-                      await ref.read(perAppProxyListProvider.notifier).update(newSelection);
-                    },
-                    secondary: package.icon == null
-                        ? null
-                        : Image.memory(
-                            package.icon!,
-                            width: 48,
-                            height: 48,
-                            cacheWidth: 48,
-                            cacheHeight: 48,
+              bottom: PreferredSize(
+                preferredSize: const Size.fromHeight(48),
+                child: Expanded(
+                  child: ListView(
+                    scrollDirection: Axis.horizontal,
+                    padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+                    children: [
+                      PopupMenuButton(
+                        borderRadius: BorderRadius.circular(8),
+                        position: PopupMenuPosition.under,
+                        tooltip: mode.present(t).message,
+                        initialValue: mode,
+                        onSelected: (e) async {
+                          await ref.read(Preferences.perAppProxyMode.notifier).update(e);
+                          if (e == PerAppProxyMode.off && context.mounted) {
+                            context.pop();
+                          }
+                        },
+                        itemBuilder: (context) => PerAppProxyMode.values
+                            .map(
+                              (e) => PopupMenuItem(
+                                value: e,
+                                child: Text(e.present(t).message),
+                              ),
+                            )
+                            .toList(),
+                        child: Container(
+                          decoration: BoxDecoration(
+                              borderRadius: BorderRadius.circular(8),
+                              color: theme.colorScheme.surface,
+                              border: Border.all(
+                                color: theme.colorScheme.outlineVariant,
+                              )),
+                          child: Row(
+                            children: [
+                              const Gap(16),
+                              Text(mode.present(t).title),
+                              const Gap(4),
+                              Icon(
+                                Icons.arrow_drop_down_rounded,
+                                color: theme.colorScheme.onSurfaceVariant,
+                              ),
+                              const Gap(8),
+                            ],
                           ),
-                  );
-                },
-                itemCount: packages.length,
-              ),
-              error: (error, _) => SliverErrorBodyPlaceholder(error.toString()),
-              loading: () => const Center(
-                child: CircularProgressIndicator(),
+                        ),
+                      ),
+                      const Gap(8),
+                      ChoiceChip(
+                        label: Text(t.settings.network.hideSystemApps),
+                        selected: hideSystemApps.value,
+                        onSelected: (value) => hideSystemApps.value = value,
+                      ),
+                    ],
+                  ),
+                ),
               ),
             ),
-          ),
-        ],
+      body: filteredPackages.when(
+        data: (packages) => ListView.builder(
+          itemBuilder: (context, index) {
+            final package = packages[index];
+            final selected = selectedApps.contains(package.packageName);
+            return CheckboxListTile(
+              title: Text(
+                package.name,
+                overflow: TextOverflow.ellipsis,
+              ),
+              subtitle: Text(
+                package.packageName,
+                style: Theme.of(context).textTheme.bodySmall,
+              ),
+              value: selected,
+              onChanged: (value) async {
+                final List<String> newSelection;
+                if (selected) {
+                  newSelection = selectedApps.exceptElement(package.packageName).toList();
+                } else {
+                  newSelection = [
+                    ...selectedApps,
+                    package.packageName,
+                  ];
+                }
+                await ref.read(selectedAppsFilteredByModeProvider.notifier).update(newSelection);
+              },
+              secondary: package.icon == null
+                  ? null
+                  : Image.memory(
+                      package.icon!,
+                      width: 48,
+                      height: 48,
+                      cacheWidth: 48,
+                      cacheHeight: 48,
+                    ),
+            );
+          },
+          itemCount: packages.length,
+        ),
+        error: (error, _) => SliverErrorBodyPlaceholder(error.toString()),
+        loading: () => const Center(
+          child: CircularProgressIndicator(),
+        ),
       ),
     );
   }
