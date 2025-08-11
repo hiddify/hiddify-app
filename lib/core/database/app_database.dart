4e6eef77268c074f0dab841a9802a66598ad6a3d
veto9292 <veto9292@gmail.com>
2025-08-11 17:36:10 +0330

    add/update/fix/remove/ adding AppProxyEntries to db tables, fixing AppDatabase constructor, adding from4To5 and increasing schemaVersion to 5, generating drit_schema_v5 with drift_dev, removing database_connection.dart

--- lib/core/database/app_database.dart
+++ lib/core/database/app_database.dart
@@ -1,32 +1,40 @@
 import 'package:drift/drift.dart';
-
+import 'package:drift_flutter/drift_flutter.dart';
 import 'package:flutter/foundation.dart';
-import 'package:hiddify/core/database/connection/database_connection.dart';
+import 'package:hiddify/core/database/app_database.steps.dart';
 import 'package:hiddify/core/database/converters/duration_converter.dart';
-import 'package:hiddify/core/database/schema_versions.dart';
 import 'package:hiddify/core/database/tables/database_tables.dart';
-
 import 'package:hiddify/features/geo_asset/model/geo_asset_entity.dart';
 import 'package:hiddify/features/profile/model/profile_entity.dart';
 import 'package:hiddify/utils/custom_loggers.dart';
 
 part 'app_database.g.dart';
 
-@DriftDatabase(tables: [ProfileEntries, GeoAssetEntries])
+@DriftDatabase(tables: [ProfileEntries, GeoAssetEntries, AppProxyEntries])
 class AppDatabase extends _$AppDatabase with InfraLogger {
-  AppDatabase({required QueryExecutor connection}) : super(connection);
-
-  AppDatabase.connect() : super(openConnection());
+  AppDatabase([QueryExecutor? executor]) : super(executor ?? _openConnection());
 
   @override
-  int get schemaVersion => 4;
+  int get schemaVersion => 5;
+
+  static QueryExecutor _openConnection() {
+    return LazyDatabase(
+      () => driftDatabase(
+        name: "db",
+        web: DriftWebOptions(
+          sqlite3Wasm: Uri.parse('sqlite3.wasm'),
+          driftWorker: Uri.parse('drift_worker.js'),
+        ),
+      ),
+    );
+  }
 
   @override
   MigrationStrategy get migration {
     return MigrationStrategy(
       onCreate: (Migrator m) async {
         await m.createAll();
-        await _prePopulateGeoAssets();
+        // await _prePopulateGeoAssets();
       },
       onUpgrade: stepByStep(
         // add type column to profile entries table
@@ -44,33 +52,36 @@ class AppDatabase extends _$AppDatabase with InfraLogger {
         },
         from2To3: (m, schema) async {
           await m.createTable(schema.geoAssetEntries);
-          await _prePopulateGeoAssets();
+          // await _prePopulateGeoAssets();
         },
         from3To4: (m, schema) async {
-          // TODO: check if column exists, if not then add column
-          try {
-            await m.addColumn(profileEntries, profileEntries.testUrl);
-          } on Exception catch (err) {
-            loggy.debug(err);
-          }
+          await m.addColumn(schema.profileEntries, schema.profileEntries.testUrl);
+        },
+        from4To5: (m, schema) async {
+          // await m.renameColumn(
+          //   schema.profileEntries,
+          //   'test_url',
+          //   schema.profileEntries.localOverride,
+          // );
+          await m.createTable(schema.appProxyEntries);
         },
       ),
       beforeOpen: (details) async {
-        if (kDebugMode) {
-          // await validateDatabaseSchema();
-        }
+        // if (kDebugMode) {
+        // await validateDatabaseSchema();
+        // }
       },
     );
   }
 
-  Future<void> _prePopulateGeoAssets() async {
-    loggy.debug("populating default geo assets");
-    await transaction(() async {
-      // final geoAssets = defaultGeoAssets.map((e) => e.toEntry());
-      // for (final geoAsset in geoAssets) {
-      //   await into(geoAssetEntries)
-      //       .insert(geoAsset, mode: InsertMode.insertOrIgnore);
-      // }
-    });
-  }
+  // Future<void> _prePopulateGeoAssets() async {
+  //   loggy.debug("populating default geo assets");
+  //   await transaction(() async {
+  //     final geoAssets = defaultGeoAssets.map((e) => e.toEntry());
+  //     for (final geoAsset in geoAssets) {
+  //       await into(geoAssetEntries)
+  //           .insert(geoAsset, mode: InsertMode.insertOrIgnore);
+  //     }
+  //   });
+  // }
 }
