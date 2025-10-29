1bcbbe7903c93ac134394ffdb3f8ab3c300a1549
veto9292 <veto9292@gmail.com>
2025-10-29 11:36:48 +0330

    synchronizing main_prod.dart with main.dart

--- lib/main_prod.dart
+++ lib/main_prod.dart
@@ -3,16 +3,11 @@ import 'package:flutter/services.dart';
 import 'package:hiddify/bootstrap.dart';
 import 'package:hiddify/core/model/environment.dart';
 
-void main() async {
+Future<void> main() async {
   final widgetsBinding = WidgetsFlutterBinding.ensureInitialized();
 
   SystemChrome.setEnabledSystemUIMode(SystemUiMode.edgeToEdge);
-  SystemChrome.setSystemUIOverlayStyle(
-    const SystemUiOverlayStyle(
-      statusBarColor: Colors.transparent,
-      systemNavigationBarColor: Colors.transparent,
-    ),
-  );
+  SystemChrome.setSystemUIOverlayStyle(const SystemUiOverlayStyle(statusBarColor: Colors.transparent, systemNavigationBarColor: Colors.transparent));
 
-  return lazyBootstrap(widgetsBinding, Environment.prod);
+  return await lazyBootstrap(widgetsBinding, Environment.prod);
 }
