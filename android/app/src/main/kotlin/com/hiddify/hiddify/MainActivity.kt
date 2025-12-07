071d011bb9580a834f9c750d70b098577f864b39
TGIR <arshia.kingforcallofduty@gmail.com>
2025-12-07 21:12:46 +0330

    Remove legacy core implementations and dependencies

--- android/app/src/main/kotlin/com/hiddify/hiddify/MainActivity.kt
+++ android/app/src/main/kotlin/com/hiddify/hiddify/MainActivity.kt
@@ -1,147 +1,15 @@
 package com.hiddify.hiddify
 
-import android.annotation.SuppressLint
-import android.content.Intent
-import android.Manifest
-import android.content.pm.PackageManager
-import android.net.VpnService
-import android.os.Build
-import android.util.Log
-import androidx.core.app.ActivityCompat
-import androidx.core.content.ContextCompat
-import androidx.lifecycle.MutableLiveData
-import androidx.lifecycle.lifecycleScope
-import androidx.activity.result.contract.ActivityResultContracts
-import com.hiddify.hiddify.bg.ServiceConnection
-import com.hiddify.hiddify.bg.ServiceNotification
-import com.hiddify.hiddify.constant.Alert
-import com.hiddify.hiddify.constant.ServiceMode
-import com.hiddify.hiddify.constant.Status
 import io.flutter.embedding.android.FlutterFragmentActivity
 import io.flutter.embedding.engine.FlutterEngine
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.launch
-import kotlinx.coroutines.withContext
-import java.util.LinkedList
-
-
-class MainActivity : FlutterFragmentActivity(), ServiceConnection.Callback {
+class MainActivity : FlutterFragmentActivity() {
     companion object {
-        private const val TAG = "ANDROID/MyActivity"
         lateinit var instance: MainActivity
-
-        const val VPN_PERMISSION_REQUEST_CODE = 1001
-        const val NOTIFICATION_PERMISSION_REQUEST_CODE = 1010
     }
-
-    private val connection = ServiceConnection(this, this)
-
-    private val vpnPermissionLauncher = registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
-        if (result.resultCode == RESULT_OK) {
-            startService()
-        } else {
-            onServiceAlert(Alert.RequestVPNPermission, null)
-        }
-    }
-
-    private val notificationPermissionLauncher = registerForActivityResult(ActivityResultContracts.RequestPermission()) { isGranted ->
-        if (isGranted) {
-            startService()
-        } else {
-            onServiceAlert(Alert.RequestNotificationPermission, null)
-        }
-    }
-
-    val logList = LinkedList<String>()
-    var logCallback: ((String) -> Unit)? = null
-    val serviceStatus = MutableLiveData(Status.Stopped)
-    val serviceAlerts = MutableLiveData<ServiceEvent?>(null)
 
     override fun configureFlutterEngine(flutterEngine: FlutterEngine) {
         super.configureFlutterEngine(flutterEngine)
         instance = this
-        reconnect()
-        flutterEngine.plugins.add(MethodHandler(lifecycleScope))
         flutterEngine.plugins.add(PlatformSettingsHandler())
-        flutterEngine.plugins.add(EventHandler())
-        flutterEngine.plugins.add(LogHandler())
-        flutterEngine.plugins.add(GroupsChannel(lifecycleScope))
-        flutterEngine.plugins.add(ActiveGroupsChannel(lifecycleScope))
-        flutterEngine.plugins.add(StatsChannel(lifecycleScope))
-    }
-
-    fun reconnect() {
-        connection.reconnect()
-    }
-
-    fun startService() {
-        if (!ServiceNotification.checkPermission()) {
-            grantNotificationPermission()
-            return
-        }
-        lifecycleScope.launch(Dispatchers.IO) {
-            if (Settings.rebuildServiceMode()) {
-                reconnect()
-            }
-            if (Settings.serviceMode == ServiceMode.VPN) {
-                if (prepare()) {
-                    Log.d(TAG, "VPN permission required")
-                    return@launch
-                }
-            }
-
-            val intent = Intent(HiddifyApp.instance, Settings.serviceClass())
-            withContext(Dispatchers.Main) {
-                ContextCompat.startForegroundService(HiddifyApp.instance, intent)
-            }
-        }
-    }
-
-    private suspend fun prepare() = withContext(Dispatchers.Main) {
-        try {
-            val intent = VpnService.prepare(this@MainActivity)
-            if (intent != null) {
-                vpnPermissionLauncher.launch(intent)
-                true
-            } else {
-                false
-            }
-        } catch (e: Exception) {
-            onServiceAlert(Alert.RequestVPNPermission, e.message)
-            false
-        }
-    }
-
-    override fun onServiceStatusChanged(status: Status) {
-        serviceStatus.postValue(status)
-    }
-
-    override fun onServiceAlert(type: Alert, message: String?) {
-        serviceAlerts.postValue(ServiceEvent(Status.Stopped, type, message))
-    }
-
-    override fun onServiceWriteLog(message: String?) {
-        if (message == null) return
-        if (logList.size > 300) {
-            logList.removeFirst()
-        }
-        logList.addLast(message)
-        logCallback?.invoke(message)
-    }
-
-    override fun onServiceResetLogs(messages: MutableList<String>) {
-        logList.clear()
-        logList.addAll(messages)
-    }
-
-    override fun onDestroy() {
-        connection.disconnect()
-        super.onDestroy()
-    }
-
-    private fun grantNotificationPermission() {
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
-            notificationPermissionLauncher.launch(Manifest.permission.POST_NOTIFICATIONS)
-        }
     }
 }
