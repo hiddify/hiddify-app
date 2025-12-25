274241330319fc659805082c8866ae3e2bf39511
veto9292 <veto9292@gmail.com>
2025-12-25 22:45:56 +0330

    Replace flutter_markdown with flutter_markdown_plus and update version in pubspec.yaml; adjust pubspec.lock accordingly.

--- lib/core/router/dialog/widgets/free_profile_consent_dialog.dart
+++ lib/core/router/dialog/widgets/free_profile_consent_dialog.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_markdown/flutter_markdown.dart';
+import 'package:flutter_markdown_plus/flutter_markdown_plus.dart';
 import 'package:go_router/go_router.dart';
 import 'package:hiddify/core/localization/translations.dart';
 import 'package:hiddify/core/model/constants.dart';
@@ -14,24 +14,19 @@ class FreeProfileConsentDialog extends HookConsumerWidget {
   Widget build(BuildContext context, WidgetRef ref) {
     final t = ref.watch(translationsProvider).requireValue;
     return AlertDialog(
-        title: Text(title),
-        content: ConstrainedBox(
-          constraints: AlertDialogConst.boxConstraints,
-          child: MarkdownBody(
-            data: consent,
-            // styleSheet: MarkdownStyleSheet(textAlign: WrapAlignment.spaceBetween),
-            onTapLink: (text, href, title) => UriUtils.tryLaunch(Uri.parse(href!)),
-          ),
+      title: Text(title),
+      content: ConstrainedBox(
+        constraints: AlertDialogConst.boxConstraints,
+        child: MarkdownBody(
+          data: consent,
+          // styleSheet: MarkdownStyleSheet(textAlign: WrapAlignment.spaceBetween),
+          onTapLink: (text, href, title) => UriUtils.tryLaunch(Uri.parse(href!)),
         ),
-        actions: [
-          TextButton(
-            child: Text(t.common.cancel),
-            onPressed: () => context.pop(false),
-          ),
-          TextButton(
-            child: Text(t.common.kContinue),
-            onPressed: () => context.pop(true),
-          ),
-        ]);
+      ),
+      actions: [
+        TextButton(child: Text(t.common.cancel), onPressed: () => context.pop(false)),
+        TextButton(child: Text(t.common.kContinue), onPressed: () => context.pop(true)),
+      ],
+    );
   }
 }
