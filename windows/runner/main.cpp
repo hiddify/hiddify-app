95914275a875f3c8ec3eb90d9e67e9945aea318f
TGIR <arshia.kingforcallofduty@gmail.com>
2026-01-31 00:44:41 +0330

    Refactor main.cpp for improved mutex handling

--- windows/runner/main.cpp
+++ windows/runner/main.cpp
@@ -1,65 +1,59 @@
 #include <flutter/dart_project.h>
 #include <flutter/flutter_view_controller.h>
 #include <windows.h>
-
 #include "flutter_window.h"
 #include "utils.h"
 
-
 int APIENTRY wWinMain(_In_ HINSTANCE instance, _In_opt_ HINSTANCE prev,
                       _In_ wchar_t *command_line, _In_ int show_command) {
-  ScopedMutex mutex(L"HiddifyMutex");
-  if (!mutex.success()) {
-    flutter::DartProject project(L"data");
-    std::vector<std::string> command_line_arguments = GetCommandLineArguments();
-    project.set_dart_entrypoint_arguments(std::move(command_line_arguments));
-    FlutterWindow window(project);
-    if (window.SendAppLinkToInstance(L"Hiddify")) {
-      return EXIT_SUCCESS;
+    
+    ScopedMutex mutex(L"HiddifyNext_Unique_Mutex_ID");
+    
+    if (!mutex.success()) {
+        flutter::DartProject project(L"data");
+        project.set_dart_entrypoint_arguments(GetCommandLineArguments());
+        FlutterWindow window(project);
+        
+        if (window.SendAppLinkToInstance(L"Hiddify")) {
+            return EXIT_SUCCESS;
+        }
+
+        HWND hwnd = ::FindWindowW(L"FLUTTER_RUNNER_WIN32_WINDOW", L"Hiddify");
+        if (hwnd) {
+            if (::IsIconic(hwnd)) ::ShowWindow(hwnd, SW_RESTORE);
+            ::SetForegroundWindow(hwnd);
+        }
+        return EXIT_SUCCESS;
     }
 
-    HWND hwnd = ::FindWindowW(nullptr, L"Hiddify");
-    if (hwnd) {
-      WINDOWPLACEMENT place = {sizeof(WINDOWPLACEMENT)};
-      GetWindowPlacement(hwnd, &place);
-      ShowWindow(hwnd, SW_NORMAL);
-      SetForegroundWindow(hwnd);
+    if (::IsDebuggerPresent()) {
+        CreateAndAttachConsole();
     }
+
+    HRESULT hr = ::CoInitializeEx(nullptr, COINIT_APARTMENTTHREADED | COINIT_DISABLE_OLE1DDE);
+
+    {
+        flutter::DartProject project(L"data");
+        project.set_dart_entrypoint_arguments(GetCommandLineArguments());
+
+        FlutterWindow window(project);
+        Win32Window::Point origin(10, 10);
+        Win32Window::Size size(1280, 720);
+
+        if (!window.Create(L"Hiddify", origin, size)) {
+            if (SUCCEEDED(hr)) ::CoUninitialize();
+            return EXIT_FAILURE;
+        }
+
+        window.SetQuitOnClose(true);
+
+        ::MSG msg;
+        while (::GetMessage(&msg, nullptr, 0, 0)) {
+            ::TranslateMessage(&msg);
+            ::DispatchMessage(&msg);
+        }
+    }
+
+    if (SUCCEEDED(hr)) ::CoUninitialize();
     return EXIT_SUCCESS;
-  }
-
-  if (!::AttachConsole(ATTACH_PARENT_PROCESS) && ::IsDebuggerPresent()) {
-    CreateAndAttachConsole();
-  }
-
-  HRESULT coinit_hresult = ::CoInitializeEx(nullptr, COINIT_APARTMENTTHREADED);
-
-  flutter::DartProject project(L"data");
-
-  std::vector<std::string> command_line_arguments =
-      GetCommandLineArguments();
-
-  project.set_dart_entrypoint_arguments(std::move(command_line_arguments));
-
-  FlutterWindow window(project);
-  Win32Window::Point origin(10, 10);
-  Win32Window::Size size(900, 700);
-  if (!window.Create(L"Hiddify", origin, size)) {
-    if (SUCCEEDED(coinit_hresult)) {
-      ::CoUninitialize();
-    }
-    return EXIT_FAILURE;
-  }
-  window.SetQuitOnClose(true);
-
-  ::MSG msg;
-  while (::GetMessage(&msg, nullptr, 0, 0)) {
-    ::TranslateMessage(&msg);
-    ::DispatchMessage(&msg);
-  }
-
-  if (SUCCEEDED(coinit_hresult)) {
-    ::CoUninitialize();
-  }
-  return EXIT_SUCCESS;
 }
